7a110bc3f31942789d730012cdaf4fbc54e758af
whale <3121279905@qq.com>
2024-08-13 17:57:22 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,3 +7,21 @@ import { FaArrowsRotate } from "react-icons/fa6";
 import { TiZoomInOutline, TiZoomOutOutline } from "react-icons/ti";
 import "./index.css"; // 确保CSS文件包含必要的样式
 import { degrees, PDFDocument } from "pdf-lib";
+
+
+
+
+export default function Home() {
+    return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="body">
+            <div style={{ padding: "20px" }} className="rotate">
+                <h1 style={{ fontSize: "3rem" }}>Rotate PDF Pages</h1>
+                <p className="tip">
+                    Simply click on a page to rotate it. You can then download your modified PDF.
+                </p>
+            </div>
+
+        </div>
+    </main>
+);}
